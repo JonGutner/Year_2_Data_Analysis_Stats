3030db26173917a6a285df2a9921d9d3df56f048
JonathanGutner <24gutnerj@nsboroschools.net>
2025-11-10 11:15:47 +0000

    Init of waves lab class, downloaded py file, and workspace.

---
+++ Waves_Lab/load_dataset.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import pandas as pd
+import re
+
+def load_dataset(path):
+    data = pd.read_csv(path, header=3)
+    timestamp = data.iloc[:, 0].to_numpy()
+    output_voltage = data.iloc[:, 1].to_numpy()
+    output_current = data.iloc[:, 2].to_numpy()
+    thermistor_temperatures = data.iloc[:, 3:].to_numpy()
+
+    comments = re.search(r"Comments: (.*)$", open(path).read(), re.MULTILINE)[1]
+
+    return timestamp, output_voltage, output_current, thermistor_temperatures, comments
+
+
+data_path = Path(__file__).resolve().parent.parent / "Waves_Lab_Data" / "Test_1.csv"
+
+timestamp, output_voltage, output_current, thermistor_temperatures, comments = load_dataset(data_path)
+print(comments)
