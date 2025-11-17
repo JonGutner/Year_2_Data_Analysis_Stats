Addition of more thermistors. Correction of phase graph, including normalisation. Correcting of frequency->period.

Year_2_Stats/helpers.py
@@ -100,27 +100,28 @@ def run_tests_waves(df, i, chosen_pdf, param_names, j):
 def run_waves_plots(packages):
     y_models_a = []
     y_models_p = []
-    d = 0.005
-    spacing = [0, d, 2 * d, 3 * d]
+    # d = 1
+    # spacing = [0, d, 2 * d, 3 * d]
+    spacing = [0, 1, 2, 3, 4, 5]
 
     for i in range(4):
         package = packages[f"package_{i}"]
         amplitude = package[0]
         phase = package[1]
-        err_a = package[2]
-        err_p = package[3]
+
+        # print ("======>", phase)
 
         params_a = [amplitude[0], -1, 0]
         popt_a, pcov_a = estimators.amplitude_fit_waves(spacing, amplitude, params_a)
         y_models_a.append(pdfs.amplitude_waves(spacing, *popt_a))
 
-        params_p = [1.5, 0]
+        params_p = [0.3, 0]
         popt_p, pcov_p = estimators.phase_fit_waves(spacing, phase, params_p)
         y_models_p.append(pdfs.phase_waves(spacing, *popt_p))
 
     outputer.show_thermistor_param(spacing, packages, y_models_a, y_models_p)
 
-def get_ampli_phase_err_waves(df_0, df_1, df_2, df_3, chosen_pdf, param_names, j):
+def get_ampli_phase_err_waves(df_0, df_1, df_2, df_3, df_4, df_5, chosen_pdf, param_names, j):
     amplitudes = []
     phases = []
     err_a = []
@@ -142,6 +143,19 @@ def get_ampli_phase_err_waves(df_0, df_1, df_2, df_3, chosen_pdf, param_names, j
     err_a.append(errs[0])
     err_p.append(errs[1])
     results, errs = run_tests_waves(df_3, 3, chosen_pdf, param_names, j)
+    amplitudes.append(np.abs(results[0]))
+    phases.append(results[1])
+    err_a.append(errs[0])
+    err_p.append(errs[1])
+
+    #EXTRA THERMISTORS:
+    results, errs = run_tests_waves(df_4, 4, chosen_pdf, param_names, j)
+    amplitudes.append(np.abs(results[0]))
+    phases.append(results[1])
+    err_a.append(errs[0])
+    err_p.append(errs[1])
+
+    results, errs = run_tests_waves(df_5, 5, chosen_pdf, param_names, j)
     amplitudes.append(np.abs(results[0]))
     phases.append(results[1])
     err_a.append(errs[0])

Year_2_Stats/main.py
@@ -1,4 +1,5 @@
 #Main method where all the setups are performed.
+import numpy as np
 from Year_2_Stats import helpers, pdfs
 from Waves_Lab import data_management
 
@@ -34,13 +35,13 @@ param_names = ["amplitude", "phase", "c"]
 
 # Load data for Waves Experiment
 data_name_1 = "period_1.csv"
-df_0_1, df_1_1, df_2_1, df_3_1 = data_management.send_data(data_name_1)
+df_0_1, df_1_1, df_2_1, df_3_1, df_4_1, df_5_1 = data_management.send_data(data_name_1)
 data_name_2 = "period_2.csv"
-df_0_2, df_1_2, df_2_2, df_3_2 = data_management.send_data(data_name_2)
+df_0_2, df_1_2, df_2_2, df_3_2, df_4_2, df_5_2 = data_management.send_data(data_name_2)
 data_name_3 = "period_3.csv"
-df_0_3, df_1_3, df_2_3, df_3_3 = data_management.send_data(data_name_3)
+df_0_3, df_1_3, df_2_3, df_3_3, df_4_3, df_5_3 = data_management.send_data(data_name_3)
 data_name_4 = "period_4.csv"
-df_0_4, df_1_4, df_2_4, df_3_4 = data_management.send_data(data_name_4)
+df_0_4, df_1_4, df_2_4, df_3_4, df_4_4, df_5_4 = data_management.send_data(data_name_4)
 
 # -----------------------------
 # Run tests
@@ -48,13 +49,20 @@ df_0_4, df_1_4, df_2_4, df_3_4 = data_management.send_data(data_name_4)
 
 # Run tests for Waves Experiment
 amplitudes_1, phases_1, err_a_1, err_p_1 = (helpers.get_ampli_phase_err_waves
-                                    (df_0_1, df_1_1, df_2_1, df_3_1, pdfs.sine_with_phase_15, param_names, 0))
+                                    (df_0_1, df_1_1, df_2_1, df_3_1, df_4_1, df_5_1, pdfs.sine_with_phase_15, param_names, 0))
 amplitudes_2, phases_2, err_a_2, err_p_2 = (helpers.get_ampli_phase_err_waves
-                                    (df_0_2, df_1_2, df_2_2, df_3_2, pdfs.sine_with_phase_20, param_names, 1))
+                                    (df_0_2, df_1_2, df_2_2, df_3_2, df_4_2, df_5_2, pdfs.sine_with_phase_20, param_names, 1))
 amplitudes_3, phases_3, err_a_3, err_p_3 = (helpers.get_ampli_phase_err_waves
-                                    (df_0_3, df_1_3, df_2_3, df_3_3, pdfs.sine_with_phase_30, param_names, 2))
+                                    (df_0_3, df_1_3, df_2_3, df_3_3, df_4_3, df_5_3, pdfs.sine_with_phase_30, param_names, 2))
 amplitudes_4, phases_4, err_a_4, err_p_4 = (helpers.get_ampli_phase_err_waves
-                                    (df_0_4, df_1_4, df_2_4, df_3_4, pdfs.sine_with_phase_60, param_names, 3))
+                                    (df_0_4, df_1_4, df_2_4, df_3_4, df_4_4, df_5_4, pdfs.sine_with_phase_60, param_names, 3))
+
+phases_1 = phases_1 - phases_1[0]
+phases_1[4] = phases_1[4] + np.pi
+phases_1[5] = phases_1[5] + np.pi
+phases_2 = phases_2 - phases_2[0]
+phases_3 = phases_3 - phases_3[0]
+phases_4 = phases_4 - phases_4[0]
 
 package_0 = [amplitudes_1, phases_1, err_a_1, err_p_1]
 package_1 = [amplitudes_2, phases_2, err_a_2, err_p_2]

Year_2_Stats/outputer.py
@@ -22,7 +22,7 @@ def print_results(name, result, param_names=None, data=None, pdf=None):
               f"(p = {gof['p_value']:.3f})")
 
 def print_results_waves(x, y, result, i, j, param_names=None, pdf=None):
-    periods = [15, 20, 30, 60]
+    periods = np.array([15, 20, 30, 60])
     print(f"\nResults for Thermistor {i} & Period {periods[j]} s:")
     for i, val in enumerate(result["params"]):
         name_str = param_names[i] if param_names else f"param{i}"
@@ -141,7 +141,7 @@ def show_fit_waves(data, pdf, params, j, t=None, bins=50, title="MLE Fit", save=
 
     # Save plot
     if save:
-        periods = [15, 20, 30, 60]
+        periods = np.array([15, 20, 30, 60])
         desktop = os.path.join(os.path.expanduser("~"), "Desktop")
         out_dir = os.path.join(desktop, "StatsPlots")
         os.makedirs(out_dir, exist_ok=True)
@@ -159,7 +159,7 @@ def show_thermistor_param(spacing, packages, y_models_a, y_models_p, save=True, 
     #Amplitude
     fig, ax = plt.subplots()
     colors = ['red', 'green', 'blue', 'orange']
-    frequencies = [15, 20, 30, 60]
+    periods = np.array([15, 20, 30, 60])
     title = "Amplitude Graphs for Thermistors 0-3"
 
     for i in range(4):
@@ -168,8 +168,8 @@ def show_thermistor_param(spacing, packages, y_models_a, y_models_p, save=True, 
         err_a = package[2]
         y_model_a = y_models_a[i]
 
-        ax.errorbar(spacing, amplitude, yerr=err_a, c=colors[i], linestyle=None,
-                    capsize=4, label=f"Frequencies {frequencies[i]}s")
+        ax.errorbar(spacing, amplitude, yerr=err_a, c=colors[i], linestyle='None',
+                    fmt='none', capsize=4, label=f"Periods {periods[i]}s")
 
         # Plot fitted model
         ax.plot(spacing, y_model_a, c=colors[i])
@@ -201,8 +201,8 @@ def show_thermistor_param(spacing, packages, y_models_a, y_models_p, save=True, 
         err_p = package[3]
         y_model_p = y_models_p[i]
 
-        ax.errorbar(spacing, phase, yerr=err_p, c=colors[i], capsize=4,linestyle=None,
-                    label=f"Frequencies {frequencies[i]}s")
+        ax.errorbar(spacing, phase, yerr=err_p, c=colors[i], linestyle='None',
+                    fmt='none', capsize=4, label=f"Periods {periods[i]}s")
 
         # Plot fitted model
         ax.plot(spacing, y_model_p, c=colors[i])