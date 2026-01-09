4c5583db36526d6caba7b8cb130d6276d7a1d157
JonathanGutner <24gutnerj@nsboroschools.net>
2026-01-09 22:13:47 +0000

    Modify to correctly state the correct energy unit.

--- Radiation_Lab/r_plotter.py
+++ Radiation_Lab/r_plotter.py
@@ -4,7 +4,7 @@ import numpy as np
 def plot_histogram(energy_files, data_names):
     for i in range(len(energy_files)):
         plt.hist(energy_files[i], bins=100)
-        plt.xlabel("Energy (eV)")
+        plt.xlabel("Energy (keV)")
         plt.ylabel("Count")
         plt.title(data_names[i])
         plt.show()
