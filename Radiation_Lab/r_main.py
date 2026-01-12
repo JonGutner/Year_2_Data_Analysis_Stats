7653abfbe57667ff13ace3a769155e36ecec751e
JonathanGutner <24gutnerj@nsboroschools.net>
2026-01-12 16:39:30 +0000

    Test commit to check branch placement.

--- Radiation_Lab/r_main.py
+++ Radiation_Lab/r_main.py
@@ -5,3 +5,4 @@ data_names = ["2MeV.txt", "2MeV_gamma.txt", "60keV_gamma.txt", "300keV.txt"] # A
 energy_files = r_helper.data_loader(data_names)
 r_helper.run_histogram(energy_files, data_names)
 
+print("Test commit")
