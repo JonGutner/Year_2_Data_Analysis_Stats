b99e374f8311dbdb2e6c6f6bbab99bc85f6e518a
JonathanGutner <24gutnerj@nsboroschools.net>
2026-01-12 16:39:42 +0000

    Revert "Test commit to check branch placement."
    This reverts commit 7653abfbe57667ff13ace3a769155e36ecec751e.

--- Radiation_Lab/r_main.py
+++ Radiation_Lab/r_main.py
@@ -5,4 +5,3 @@ data_names = ["2MeV.txt", "2MeV_gamma.txt", "60keV_gamma.txt", "300keV.txt"] # A
 energy_files = r_helper.data_loader(data_names)
 r_helper.run_histogram(energy_files, data_names)
 
-print("Test commit")
